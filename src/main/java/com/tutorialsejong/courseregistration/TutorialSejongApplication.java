63446ab81feb607fc30054b533ffeda556d8481f
anhye0n <anhye0ndev@gmail.com>
2024-07-11 11:42:22 +0900

    chore: 초기 설정

---
+++ src/main/java/com/tutorialsejong/courseregistration/TutorialSejongApplication.java
@@ -0,0 +1,13 @@
+package com.tutorialsejong.courseregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSejongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialSejongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutorialsejong/courseregistration/TutorialSejongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorialsejong.courseregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSejongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
