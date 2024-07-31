515c68ff0c9e660f43faa557147c006ea1d1b3f5
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-07-31 09:32:25 +0900

    feat: 관심과목 삭제 api 구현

--- src/main/java/com/tutorialsejong/courseregistration/wishlist/repository/WishListRepository.java
+++ src/main/java/com/tutorialsejong/courseregistration/wishlist/repository/WishListRepository.java
@@ -1,12 +1,15 @@
 package com.tutorialsejong.courseregistration.wishlist.repository;
 
-import com.tutorialsejong.courseregistration.wishlist.entity.WishList;
+import com.tutorialsejong.courseregistration.schedule.entity.Schedule;
 import com.tutorialsejong.courseregistration.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.tutorialsejong.courseregistration.wishlist.entity.WishList;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishListRepository extends JpaRepository<WishList, String> {
 
     List<WishList> findAllByStudentId(User studentId);
+
+    Optional<WishList> findByStudentIdAndScheduleId(User user, Schedule schedule);
 }
