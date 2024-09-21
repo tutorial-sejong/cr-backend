5a94160e9bacc8981c19fa79a537412134f4c8fa
anhye0n <ajhrc08070@gmail.com>
2024-09-21 13:30:34 +0900

    chore: swagger 기본 설정

--- src/main/java/com/tutorialsejong/courseregistration/common/config/SecurityConfig.java
+++ src/main/java/com/tutorialsejong/courseregistration/common/config/SecurityConfig.java
@@ -55,6 +55,8 @@ public class SecurityConfig {
                                 "/api/auth/refresh",
                                 "/api/auth/withdrawal/**"
                         ).permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exceptionHandling -> exceptionHandling
