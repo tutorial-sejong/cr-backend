fa11c2e0e792d5c50fcab9bf682219b0007cd235
anhye0n <anhye0ndev@gmail.com>
2025-01-20 14:17:36 +0900

    chore: wishlist 도메인 swagger 설정

--- src/main/java/com/tutorialsejong/courseregistration/domain/wishlist/controller/WishListController.java
+++ src/main/java/com/tutorialsejong/courseregistration/domain/wishlist/controller/WishListController.java
@@ -1,8 +1,14 @@
 package com.tutorialsejong.courseregistration.domain.wishlist.controller;
 
+import com.tutorialsejong.courseregistration.domain.schedule.entity.Schedule;
 import com.tutorialsejong.courseregistration.domain.wishlist.dto.WishListRequest;
 import com.tutorialsejong.courseregistration.domain.wishlist.service.WishListService;
-import com.tutorialsejong.courseregistration.domain.schedule.entity.Schedule;
+import com.tutorialsejong.courseregistration.domain.wishlist.swagger.DeleteWishListOperation;
+import com.tutorialsejong.courseregistration.domain.wishlist.swagger.GetWishListOperation;
+import com.tutorialsejong.courseregistration.domain.wishlist.swagger.SaveWishListOperation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,33 +18,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/wishlist")
+@Tag(name = "관심과목 API", description = "관심과목 관련 API")
 public class WishListController {
 
     private final WishListService wishListService;
 
-    @Autowired
     public WishListController(WishListService wishListService) {
         this.wishListService = wishListService;
     }
 
-
+    @SaveWishListOperation
     @PostMapping("/save")
     public ResponseEntity<?> saveWishListItem(@RequestBody WishListRequest wishListRequest) {
         wishListService.saveWishListItem(wishListRequest.studentId(), wishListRequest.scheduleId());
-
         return ResponseEntity.status(HttpStatus.CREATED).body("관심과목이 저장되었습니다.");
     }
 
-    @GetMapping()
+    @GetWishListOperation
+    @GetMapping
     public ResponseEntity<?> getWishList(@RequestParam String studentId) {
         List<Schedule> wishList = wishListService.getWishList(studentId);
-
-        return ResponseEntity.status(HttpStatus.OK).body(wishList);
+        return ResponseEntity.ok(wishList);
     }
 
+    @DeleteWishListOperation
     @DeleteMapping
-    public ResponseEntity<?> deleteWishListItem(@RequestParam String studentId, @RequestParam Long scheduleId) {
+    public ResponseEntity<?> deleteWishListItem(
+            @Parameter(description = "11자리 이상 학번", example = "12345678911")
+            @RequestParam String studentId,
+
+            @Parameter(description = "강의 ID", example = "1")
+            @RequestParam Long scheduleId
+    ) {
         wishListService.deleteWishListItem(studentId, scheduleId);
-        return ResponseEntity.status(HttpStatus.OK).body("관심과목이 삭제되었습니다.");
+        return ResponseEntity.ok("관심과목이 삭제되었습니다.");
     }
 }
--- src/main/java/com/tutorialsejong/courseregistration/domain/wishlist/swagger/GetWishListOperation.java
+++ src/main/java/com/tutorialsejong/courseregistration/domain/wishlist/swagger/GetWishListOperation.java
@@ -1,4 +1,30 @@
 package com.tutorialsejong.courseregistration.domain.wishlist.swagger;
 
-public interface GetWishListOperation {
+import com.tutorialsejong.courseregistration.domain.registration.dto.CourseRegistrationScheduleResponse;
+import com.tutorialsejong.courseregistration.domain.schedule.entity.Schedule;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "관심과목 조회", description = "특정 학생의 관심 과목 목록을 조회한다.")
+@ApiResponses(value = {
+        @ApiResponse(
+                responseCode = "200",
+                description = "조회 성공",
+                content = @Content(
+                        mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        array = @ArraySchema(schema = @Schema(implementation = CourseRegistrationScheduleResponse.class))
+                )
+        )
+})
+public @interface GetWishListOperation {
 }
