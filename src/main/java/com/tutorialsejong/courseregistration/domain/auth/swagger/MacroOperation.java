08c31f89d35d9fcfae4ad44c946421955b123e10
anhye0n <anhye0ndev@gmail.com>
2025-01-20 14:16:35 +0900

    chore: Auth 도메인 swagger 설정

--- src/main/java/com/tutorialsejong/courseregistration/domain/auth/swagger/MacroOperation.java
+++ src/main/java/com/tutorialsejong/courseregistration/domain/auth/swagger/MacroOperation.java
@@ -1,4 +1,34 @@
 package com.tutorialsejong.courseregistration.domain.auth.swagger;
 
-public class MacroOperation {
+import com.tutorialsejong.courseregistration.common.exception.ErrorResponse;
+import com.tutorialsejong.courseregistration.common.security.exception.SecurityErrorCode;
+import com.tutorialsejong.courseregistration.domain.auth.dto.MacroResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.MediaType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "매크로 코드", description = "매크로 코드 발급", security = @SecurityRequirement(name = "BearerAuth"))
+@ApiResponses(value = {
+        @ApiResponse(
+                responseCode = "200",
+                description = "성공",
+                content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = MacroResponse.class)
+                )
+        )
+})
+@SecurityRequirement(name="jwt")
+public @interface MacroOperation {
 }
--- src/main/java/com/tutorialsejong/courseregistration/domain/auth/swagger/WithdrawalOperation.java
+++ src/main/java/com/tutorialsejong/courseregistration/domain/auth/swagger/WithdrawalOperation.java
@@ -1,4 +1,38 @@
 package com.tutorialsejong.courseregistration.domain.auth.swagger;
+import com.tutorialsejong.courseregistration.domain.auth.dto.LoginResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 
-public interface WithdrawalOperation {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "회원 탈퇴", description = "회원 탈퇴 api")
+@Parameters({
+        @Parameter(
+                name = "studentId",
+                description = "탈퇴할 유저의 학번",
+                required = true,
+                example = "12345678911",
+                in = ParameterIn.PATH
+        )
+})
+@ApiResponses(value = {
+        @ApiResponse(
+                responseCode = "200",
+                description = "성공",
+                content = @Content()
+        )
+})
+public @interface WithdrawalOperation {
 }
